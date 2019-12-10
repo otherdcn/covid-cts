88c111473620134782ef5ae75c8a9f9d5ad9da4a
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-10 23:19:15 +0200

    Scaffold Category and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_211247) do
+ActiveRecord::Schema.define(version: 2019_12_10_211734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "title"
