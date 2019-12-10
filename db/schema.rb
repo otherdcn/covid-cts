f3a66b418d02cc66481dd1d6bb421fed4c44eae7
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-10 23:21:41 +0200

    Scaffold Field and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_211734) do
+ActiveRecord::Schema.define(version: 2019_12_10_211941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "category", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fields", force: :cascade do |t|
+    t.string "field", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
