97ab40814b59ac682e63f96d41cf8f808ba93dde
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-11 15:32:34 +0200

    Add form fields, table view and strong parameters to Skill

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -69,6 +69,7 @@ class SkillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def skill_params
-      params.fetch(:skill, {})
+      params.require(:skill).permit(:skill_type_id, :rating, :person_id)
+      #params.fetch(:skill, {})
     end
 end
