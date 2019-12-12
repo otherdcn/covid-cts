4ae6660567e4dc47e8c643f9a640cd0d717616e1
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-12 18:20:26 +0200

    Functionality checkup 1 for Skills and Competencies

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,4 +3,17 @@ class Skill < ApplicationRecord
 
     belongs_to :person
     validates :person_id, :skill_type_id, :rating, presence: true
+
+    def get_rating
+        case rating
+        when 1
+            return "Excellent"
+        when 2
+            return "Good"
+        when 3
+            return "Average"
+        else
+            return "Unknown"
+        end
+    end
 end
