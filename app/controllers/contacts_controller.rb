44d64abaff23c08ed526dee78c477181c7cd8943
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-12 17:57:01 +0200

    Functionality checkup 1 for Contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.json
   def index
-    @contacts = @person.contact.all
+    @contacts = @person.contact
   end
 
   # GET /contacts/1
