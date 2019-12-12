b511d698a3e2b45aef7cc100dd72a413b06f2e2c
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-12 18:40:43 +0200

    Minor improvements with Language proficiency

--- app/models/language_proficiency.rb
+++ app/models/language_proficiency.rb
@@ -2,4 +2,44 @@ class LanguageProficiency < ApplicationRecord
     belongs_to :person
     
     validates :person_id, :language, :read, :write, :speak, presence: true
+
+    def get_read_rating
+        case read
+        when 1
+            return "Beginner"
+        when 2
+            return "Intermediate"
+        when 3
+            return "Advanced"
+        else
+            return "Unknown"
+        end
+    end
+
+    def get_write_rating
+        case write
+        when 1
+            return "Beginner"
+        when 2
+            return "Intermediate"
+        when 3
+            return "Advanced"
+        else
+            return "Unknown"
+        end
+    end
+
+    def get_speak_rating
+        case speak
+        when 1
+            return "Beginner"
+        when 2
+            return "Intermediate"
+        when 3
+            return "Advanced"
+        else
+            return "Unknown"
+        end
+    end
+
 end
