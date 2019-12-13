99c0690c49cda05ae08da633884b4cc5ea2d9122
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-13 13:07:33 +0200

    Add component to view all professional memberships

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
   resources :all_qualifications
   resources :all_specialisations
   resources :all_skills
+  resources :all_professional_memberships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
