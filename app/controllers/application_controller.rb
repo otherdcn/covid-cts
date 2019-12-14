89d7f1a6500c23dce9c630f53a070e01dc2e903e
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-14 19:55:35 +0200

    Add Sessions controller and edit header navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    before_action :require_login
+    helper_method :current_user
+  
+    def current_user
+     if session[:user_id]
+       @current_user ||= User.find(session[:user_id])
+     else
+       @current_user = nil
+     end
+    end
+  
+    def logged_in?
+      !current_user.nil?
+    end
+  
+    private
+      def require_login
+        unless logged_in?
+          flash[:error] = "You must be logged in to access this section"
+          redirect_to root_path # halts request cycle
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,17 @@ Rails.application.routes.draw do
   resources :fields
   resources :categories
   resources :sectors
+  
   resources :all_qualifications
   resources :all_specialisations
   resources :all_skills
   resources :all_professional_memberships
+  
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
