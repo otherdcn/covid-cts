a895a3cb9f8e8eb1f7016bfb81e65efa3e964ac0
Oz-42 <danny.c.ntwari@gmail.com>
2019-12-17 10:08:19 +0200

    Encapsulate views elements under container class, and redirect to login instead of rendering /sessions/new for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password is invalid"
-      render "new"
+      redirect_to login_path, alert: "Email or password is invalid"
     end
   end
 
