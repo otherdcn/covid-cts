51e8964c034bde640750d8538eff11ef8d44fdc6
Oz-42 <danny.c.ntwari@gmail.com>
2020-03-06 15:43:19 +0200

    Change some details in capistrano files

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/home/nipam/#{fetch :application}"
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '10.24.154.33', user: "nipam", roles: %w{app db web}
+server '10.24.154.33', user: "nipam", roles: %w{app db web}, my_property: :my_value
 
 
 # role-based syntax
