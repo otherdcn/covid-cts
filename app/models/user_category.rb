10dec90686d2e6b62879a35d5bc7c46a38e5928c
otherdcn <danny.c.ntwari@gmail.com>
2020-07-24 13:02:46 +0200

    Remove uniqueness validation for now

--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -1,5 +1,7 @@
 class UserCategory < ApplicationRecord
   belongs_to :person
   
-  validates :category, uniqueness: {case_sensitive: false}
+  # validates :category, uniqueness: {case_sensitive: false}
+  # The above validation will prevent user category to be saved to any other person even if
+  # that person does not have the category, cause it's already saved by someone else.
 end
